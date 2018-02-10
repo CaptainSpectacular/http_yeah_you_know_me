9c87b019f2449d2c3afc943c2517203ecb7f9739
pollockcl <pollock.clee@gmail.com>
2018-02-10 10:31:08 -0700

    some refactoring. tests are passing as long as runner file is running.

--- test/daemon_test.rb
+++ test/daemon_test.rb
@@ -2,23 +2,20 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require 'socket'
 require 'faraday'
-require './lib/daemon'
+require './lib/runner'
 
-class WebServerTest < Minitest::Test
+class DaemonTest < Minitest::Test
 
   def test_daemon_exists_with_port
-    daemon = Daemon.new(9292)
+    daemon = Daemon.new(9191)
 
     assert_instance_of Daemon, daemon
-    assert_equal 9292, daemon.port
+    assert_equal 9191, daemon.port
   end
 
   def test_daemon_listens_and_provides_feedback
-    daemon = Daemon.new(9292)
-    daemon.listen
-
-    assert_equal "Hello World! (0)", Faraday.get('http://localhost:9292').body
-    assert_equal "Hello World! (1)", Faraday.get('http://localhost:9292').body
-    assert_equal "Hello World! (2)", Faraday.get('http://localhost:9292').body
+    assert_equal 'Hello World! (0)', Faraday.get('http://localhost:9292').body
+    assert_equal 'Hello World! (1)', Faraday.get('http://localhost:9292').body
+    assert_equal 'Hello World! (2)', Faraday.get('http://localhost:9292').body
   end
 end
