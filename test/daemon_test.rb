1836c362da05289c361d177c1824ada4fc765009
pollockcl <pollock.clee@gmail.com>
2018-02-10 14:36:05 -0700

    had to change some of the tests for iteration 1. wrote new test for diagnostics.

--- test/daemon_test.rb
+++ test/daemon_test.rb
@@ -14,8 +14,25 @@ class DaemonTest < Minitest::Test
   end
 
   def test_daemon_listens_and_provides_feedback
-    assert_equal 'Hello World! (0)', Faraday.get('http://localhost:9292').body
-    assert_equal 'Hello World! (1)', Faraday.get('http://localhost:9292').body
-    assert_equal 'Hello World! (2)', Faraday.get('http://localhost:9292').body
+    assert_equal 'Hello World! (0)', Faraday.get('http://localhost:9292').body.split("\n")[0]
+    assert_equal 'Hello World! (1)', Faraday.get('http://localhost:9292').body.split("\n")[0]
+    assert_equal 'Hello World! (2)', Faraday.get('http://localhost:9292').body.split("\n")[0]
+  end
+
+  def test_footer_has_diagnostics
+    expected = ['Verb: GET',
+                'Path: /',
+                'Protocol: HTTP/1.1',
+                'Host: localhost:9292',
+                'Port: 9292',
+                'Origin: 127.0.0.1',
+                'Accept: */*']
+    
+    assert_equal expected[0], Faraday.get('http://localhost:9292').body.split("\n")[1]
+    assert_equal expected[1], Faraday.get('http://localhost:9292').body.split("\n")[2]
+    assert_equal expected[2], Faraday.get('http://localhost:9292').body.split("\n")[3]
+    assert_equal expected[3], Faraday.get('http://localhost:9292').body.split("\n")[4]
+    assert_equal expected[4], Faraday.get('http://localhost:9292').body.split("\n")[5]
+    assert_equal expected[5], Faraday.get('http://localhost:9292').body.split("\n")[6]
   end
 end
