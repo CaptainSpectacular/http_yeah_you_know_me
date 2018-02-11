e9e31b6159150285cf87bad4163c0aee1a982119
pollockcl <pollock.clee@gmail.com>
2018-02-11 11:24:39 -0700

    broke all my tests. need to find a better testing method

--- test/daemon_test.rb
+++ test/daemon_test.rb
@@ -14,9 +14,9 @@ class DaemonTest < Minitest::Test
   end
 
   def test_daemon_listens_and_provides_feedback
-    assert_equal 'Hello World! (0)', Faraday.get('http://localhost:9292').body.split("\n")[0]
-    assert_equal 'Hello World! (1)', Faraday.get('http://localhost:9292').body.split("\n")[0]
-    assert_equal 'Hello World! (2)', Faraday.get('http://localhost:9292').body.split("\n")[0]
+    assert_equal 'Hello World! (0)', Faraday.get('http://localhost/hello:9292').body.split("\n")[0]
+    assert_equal 'Hello World! (1)', Faraday.get('http://localhost/hello:9292').body.split("\n")[0]
+    assert_equal 'Hello World! (2)', Faraday.get('http://localhost/hello:9292').body.split("\n")[0]
   end
 
   def test_footer_has_diagnostics
@@ -28,19 +28,17 @@ class DaemonTest < Minitest::Test
                 'Origin: 127.0.0.1',
                 'Accept: */*']
     
-    assert_equal expected[0], Faraday.get('http://localhost:9292').body.split("\n")[1]
-    assert_equal expected[1], Faraday.get('http://localhost:9292').body.split("\n")[2]
-    assert_equal expected[2], Faraday.get('http://localhost:9292').body.split("\n")[3]
-    assert_equal expected[3], Faraday.get('http://localhost:9292').body.split("\n")[4]
-    assert_equal expected[4], Faraday.get('http://localhost:9292').body.split("\n")[5]
-    assert_equal expected[5], Faraday.get('http://localhost:9292').body.split("\n")[6]
+    assert_equal expected[0], Faraday.get('http://localhost/diagnostics:9292').body.split("\n")[1]
+    assert_equal expected[1], Faraday.get('http://localhost/diagnostics:9292').body.split("\n")[2]
+    assert_equal expected[2], Faraday.get('http://localhost/diagnostics:9292').body.split("\n")[3]
+    assert_equal expected[3], Faraday.get('http://localhost/diagnostics:9292').body.split("\n")[4]
+    assert_equal expected[4], Faraday.get('http://localhost/diagnostics:9292').body.split("\n")[5]
+    assert_equal expected[5], Faraday.get('http://localhost/diagnostics:9292').body.split("\n")[6]
   end
 
-  def test_routes_between_pages
-    assert_equal 'Hello World! (0)', Faraday.get('http://localhost:9292/hello').body
-    assert_equal 'Verb: GET', Faraday.get('http://localhost:9292').body.split("\n")[0]
-    assert_equal 'somehow test the date and time', Faraday.get(('http://localhost:9292/datetime').body.split("\n")[0]
-    assert_equal '4', Faraday.get(('http://localhost:9292/shutdown').body.split("\n")[0]
+  def test_datetime_and_shutdown_pages
+    assert, Faraday.get('http://localhost:9292/datetime').body.split("\n")[0].include?('February')
+    assert_equal '4', Faraday.get('http://localhost:9292/shutdown').body.split("\n")[0]
     # test that the server is no longer operational after /shutdown
   end
 end
