88936db35d1e4fae2a937fb484e5f82d0b87ca87
pollockcl <pollock.clee@gmail.com>
2018-02-12 14:20:53 -0700

    starging over. didnt like the flow and design

--- lib/global.rb
+++
@@ -1,8 +0,0 @@
-module Global
-  class << self
-    attr_accessor :hello, :net_requests
-  end
-end
-
-Global.hello        = -1
-Global.net_requests = 0
--- lib/runner.rb
+++
@@ -1,8 +0,0 @@
-require_relative './daemon'
-
-class Runner
-  def self.start
-    daemon = Daemon.new(9292)
-    daemon.listen 
-  end
-end
