c68697615107427943f320a71b02555fc1e65dc9
pollockcl <pollock.clee@gmail.com>
2018-02-12 15:23:49 -0700

    no real changes

--- test/server_test.rb
+++ test/server_test.rb
@@ -15,7 +15,6 @@ class ServerTest < Minitest::Test
   def test_server_listens_on_port
     server = Server.new(9191)
 
-
     assert_equal 'Hello World (0)', Faraday.get('http://localhost:9292').body
     assert_equal 'Hello World (1)', Faraday.get('http://localhost:9292').body
     assert_equal 'Hello World (2)', Faraday.get('http://localhost:9292').body
