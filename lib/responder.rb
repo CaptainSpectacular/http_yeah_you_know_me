f30a2c584757e3ae5f5624a9b7b938e62e4b5777
pollockcl <pollock.clee@gmail.com>
2018-02-12 16:49:38 -0700

    some changes to work with the parser class

--- lib/responder.rb
+++ lib/responder.rb
@@ -1,8 +1,26 @@
 require './lib/tracker'
+require './lib/parser'
 class Responder
 
   def self.respond(request)
-    hello
+    @parser = Parser.new(request)
+
+    case @parser.path
+    when '/'     then diagnostics
+    when 'hello' then hello
+    end
+  end
+
+  def self.diagnostics
+    <<~HEREDOC
+      Verb: #{@parser.verb}
+      Path: #{@parser.path}
+      Protocol: #{@parser.protocol}
+      Host: #{@parser.host}
+      Port: #{@parser.port}
+      Origin: #{@parser.origin}
+      Accept: #{@parser.origin}
+    HEREDOC
   end
 
   def self.hello
