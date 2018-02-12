adc6eae59439afb6306c55ea8310d0a23c054851
pollockcl <pollock.clee@gmail.com>
2018-02-12 10:13:06 -0700

    removed guess_total from global vars

--- lib/global.rb
+++ lib/global.rb
@@ -1,9 +1,8 @@
 module Global
   class << self
-    attr_accessor :hello, :net_requests, :guess_total
+    attr_accessor :hello, :net_requests
   end
 end
 
 Global.hello        = -1
 Global.net_requests = 0
-Global.guess_total  = 0
