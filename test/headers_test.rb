78c5b8b5ef8115d6ba36e2c980598b2c13e2fcee
pollockcl <pollock.clee@gmail.com>
2018-02-12 15:29:27 -0700

    tests for the headers

---
+++ test/headers_test.rb
@@ -0,0 +1,16 @@
+require './lib/headers'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class HeadersTest < Minitest::Test
+
+  def test_default_headers
+    expected = ["http/1.1 200 ok",
+                "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
+                "server: ruby",
+                "content-type: text/html; charset=iso-8859-1",
+                "content-length: 12\r\n\r\n"].join("\r\n")
+
+    assert_equal expected, Headers.default('twelve chars')
+  end
+end
