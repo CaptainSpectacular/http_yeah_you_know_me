20b084972b25b651c311c19c4a70ed872c30fb1f
pollockcl <pollock.clee@gmail.com>
2018-02-12 15:22:13 -0700

    made class to handle headers

---
+++ lib/headers.rb
@@ -0,0 +1,11 @@
+class Headers
+
+  def self.default(response)
+    headers = ["http/1.1 200 ok",
+          "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
+          "server: ruby",
+          "content-type: text/html; charset=iso-8859-1",
+          "content-length: #{response.length}\r\n\r\n"].join("\r\n")
+  end
+
+end
