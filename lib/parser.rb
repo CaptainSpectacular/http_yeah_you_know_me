183449fb663af2a906e9ab2d93255b15dbb83034
pollockcl <pollock.clee@gmail.com>
2018-02-12 16:49:12 -0700

    added a parser class to handle data mutilation

---
+++ lib/parser.rb
@@ -0,0 +1,18 @@
+class Parser
+  attr_reader :verb, :path, :protocol, :host, :port, :origin, :accept
+
+  def initialize(request)
+    @verb, @path, @protocol = request[0].split
+
+    request.each do |line|
+      key, value = line.split(': ')
+      case key
+      when 'Host'   then @host   = value
+      when 'Accept' then @accept = value
+      end
+    end
+
+    @origin, @port = @host.split(':')
+  end
+
+end
