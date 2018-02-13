f2137cdfbb12b06df6c6f5613b8ac9089202b549
pollockcl <pollock.clee@gmail.com>
2018-02-13 08:41:18 -0700

    added and changed some tests for word_search

--- test/responder_test.rb
+++ test/responder_test.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/responder'
 require './lib/tracker'
+require './lib/parser'
 
 class ResponderTest < Minitest::Test
 
@@ -40,8 +41,20 @@ class ResponderTest < Minitest::Test
   end
 
   def test_shut_down
-    expected = 'Total Requests: 0'
+    expected = 'Total Requests:'
 
-    assert_equal expected, Responder.shut_down
+    assert Responder.shut_down.include?(expected)
+  end
+
+  def test_word_search
+    mock = ["GET /word_search?word=spizzerinctum HTTP/1.1",
+            "User-Agent: Faraday v0.14.0",
+            "Accept-Encoding: gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
+            "Accept: */*",
+            "Connection: close",
+            "Host: localhost:9292",
+            "GET / HTTP/1.1"]
+
+    assert_equal 'SPIZZERINCTUM is a known word', Responder.respond(mock)
   end
 end
