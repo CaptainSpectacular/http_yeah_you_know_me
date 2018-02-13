e8a7a2c27e4ae63852e8769313ad14f57c7ba8ae
pollockcl <pollock.clee@gmail.com>
2018-02-12 17:13:37 -0700

    added tests for the diagnostic page

--- test/responder_test.rb
+++ test/responder_test.rb
@@ -10,4 +10,26 @@ class ResponderTest < Minitest::Test
 
     assert_equal expected, Responder.hello
   end
+
+  def test_diagnostics
+    mock = ["GET / HTTP/1.1",
+            "User-Agent: Faraday v0.14.0",
+            "Accept-Encoding: gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
+            "Accept: */*",
+            "Connection: close",
+            "Host: localhost:9292",
+            "GET / HTTP/1.1"]
+
+    expected = <<~HEREDOC
+                Verb: GET
+                Path: /
+                Protocol: HTTP/1.1
+                Host: localhost:9292
+                Port: 9292
+                Origin: localhost
+                Accept: */*
+               HEREDOC
+
+    assert_equal expected, Responder.respond(mock)
+  end
 end
