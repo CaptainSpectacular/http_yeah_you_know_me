366ab8818330ab85adff7230c46776a31dff262c
pollockcl <pollock.clee@gmail.com>
2018-02-13 12:03:44 -0700

    dividing between GET and POST requests

--- lib/router.rb
+++ lib/router.rb
@@ -4,8 +4,15 @@ require './lib/parser'
 class Router
 
   def self.route(request)
-    Tracker.total_reqs += 1
     @parser = Parser.new(request)
+    case @parser.verb
+    when 'GET'  then route_get
+    when 'POST' then route_post
+    end
+  end
+
+  def self.route_get
+    Tracker.total_reqs += 1
 
     return Responder.word_search(@parser) if @parser.path.include?('/word_search')
 
@@ -15,6 +22,10 @@ class Router
     when '/datetime' then Responder.date_time
     when '/shutdown' then Responder.shut_down
     end
+  end
+
+  def self.route_post
+  
   end
 
 end
