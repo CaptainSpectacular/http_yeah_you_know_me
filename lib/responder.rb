b8e110e436d90b3d2a3f01c9aecdf2ca26406cfe
pollockcl <pollock.clee@gmail.com>
2018-02-13 08:16:11 -0700

    added functional word_search method

--- lib/responder.rb
+++ lib/responder.rb
@@ -1,10 +1,12 @@
 require './lib/tracker'
 require './lib/parser'
+require 'pry'
 class Responder
 
   def self.respond(request)
     Tracker.total_reqs += 1
     @parser = Parser.new(request)
+    return word_search if @parser.path.include?('/word_search')
 
     case @parser.path
     when '/'         then diagnostics
@@ -37,5 +39,15 @@ class Responder
   def self.hello
     Tracker.hellos += 1
     "Hello World (#{Tracker.hellos})"
+  end
+
+  def self.word_search
+    word = @parser.find_word
+    dict = File.read('/usr/share/dict/words')
+
+    case dict.include?(word)
+    when true  then "#{word} is a known word"
+    when false then "#{word} is not a known word"
+    end
   end
 end
