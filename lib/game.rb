95c7e1b72b8ef4c5ca125c4079ec96ae1dfd7bd5
pollockcl <pollock.clee@gmail.com>
2018-02-13 14:56:29 -0700

    slight changes. want the responder to handle the feedback

--- lib/game.rb
+++ lib/game.rb
@@ -1,17 +1,20 @@
 class Game
   attr_accessor :master_number
-  attr_reader :feedback
+  attr_reader :recent_guess, :feedback, :guess_total
   def initialize
     @master_number = rand(1..100)
+    @guess_total   = 0
+    @recent_guess  = nil
     @feedback      = nil
   end
 
   def guess(num)
+    @guess_total += 1
+    @recent_guess = num
     case num <=> @master_number
-    when  1 then @feedback = "#{num} is too high!"
-    when  0 then @feedback = "That's the right number!"
-    when -1 then @feedback = "#{num} is too low!"
+    when  1 then @feedback = 'Too high!'
+    when  0 then @feedback = 'That\'s correct!'
+    when -1 then @feedback = 'Too low!'
     end
   end
-
 end
