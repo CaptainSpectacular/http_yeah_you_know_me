cb7f62e35e098297c330b6ec0c63ab78778eda4e
pollockcl <pollock.clee@gmail.com>
2018-02-13 14:57:53 -0700

    keeps track of the game

--- lib/tracker.rb
+++ lib/tracker.rb
@@ -1,8 +1,10 @@
+require './lib/game'
 module Tracker
   class << self
-    attr_accessor :hellos, :total_reqs
+    attr_accessor :hellos, :total_reqs, :game
   end
 end
 
 Tracker.hellos     = -1
 Tracker.total_reqs = 0
+Tracker.game       = nil
