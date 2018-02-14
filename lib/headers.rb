dc816b55f8e2f483aea0dc7204317f556477efd8
pollockcl <pollock.clee@gmail.com>
2018-02-14 14:35:57 -0700

    redesigned to be dynamic

--- lib/headers.rb
+++ lib/headers.rb
@@ -1,10 +1,17 @@
 class Headers
 
-  def self.default(response)
-    headers = ["http/1.1 200 ok",
-          "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
-          "server: ruby",
-          "content-type: text/html; charset=iso-8859-1",
-          "content-length: #{response.length}\r\n\r\n"].join("\r\n")
+  def self.headers(response, tag)
+    paths = { ok:           '200 ok',
+              not_found:    '404 Not Found',
+              forbidden:    '403 Forbidden',
+              moved:        '301 Moved Permanently',
+              error:        '500 Internal Server Error'}
+
+    ["http/1.1 #{paths[tag]}",
+    "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
+    "server: ruby",
+    "content-type: text/html; charset=iso-8859-1",
+    "content-length: #{response.length}\r\n\r\n"].join("\r\n")
+    
   end
 end
