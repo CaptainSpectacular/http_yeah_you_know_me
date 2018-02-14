719da3574d3fe5c7391d9f24551db9ab1c12d7cd
pollockcl <pollock.clee@gmail.com>
2018-02-13 17:51:05 -0700

    added find_guess method

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,5 +1,5 @@
 class Parser
-  attr_reader :verb, :path, :protocol, :host, :port, :origin, :accept
+  attr_reader :verb, :path, :protocol, :host, :port, :origin, :accept, :content_length
 
   def initialize(request)
     @verb, @path, @protocol = request[0].split
@@ -7,8 +7,9 @@ class Parser
     request.each do |line|
       key, value = line.split(': ')
       case key
-      when 'Host'   then @host   = value
-      when 'Accept' then @accept = value
+      when 'Host'           then @host   = value
+      when 'Accept'         then @accept = value
+      when 'Content-Length' then @content_length = value.to_i
       end
     end
 
@@ -17,6 +18,14 @@ class Parser
 
   def find_word
     path.scan(/\=\w*/)[0].delete('=')
+  end
+
+  def find_guess(request)
+    body = request.read(content_length)
+    guess = body.scan(/\n\d\d?\r/m)[0]
+    guess.delete!("\n")
+    guess.delete!("\r")
+    guess.to_i
   end
 
 end
