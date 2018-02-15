6d50fc77695927801cf6c606d23eba55652712ed
pollockcl <pollock.clee@gmail.com>
2018-02-15 13:52:18 -0700

    added tests for failure pages

--- test/responder_test.rb
+++ test/responder_test.rb
@@ -47,19 +47,27 @@ class ResponderTest < Minitest::Test
   end
 
   def test_word_search
-    mock = ["GET /word_search?word=spizzerinctum HTTP/1.1",
+    mock_1 = ["GET /word_search?word=spizzerinctum HTTP/1.1",
             "User-Agent: Faraday v0.14.0",
             "Accept-Encoding: gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
             "Accept: */*",
             "Connection: close",
             "Host: localhost:9292",
             "GET / HTTP/1.1"]
-
-    assert_equal 'SPIZZERINCTUM is a known word', Router.route(mock)
+    mock_2 = ["GET /word_search?word=farquad HTTP/1.1",
+              "User-Agent: Faraday v0.14.0",
+              "Accept-Encoding: gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
+              "Accept: */*",
+              "Connection: close",
+              "Host: localhost:9292",
+              "GET / HTTP/1.1"]
+  
+    assert_equal 'SPIZZERINCTUM is a known word', Router.route(mock_1)
+    assert_equal 'FARQUAD is not a known word', Router.route(mock_2)
   end
 
   def test_game_start
-    assert_equal 'Good luck!', Responder.start_game
+    assert_equal ['Good luck!', :moved], Responder.start_game
   end
 
   def test_game
@@ -71,5 +79,17 @@ class ResponderTest < Minitest::Test
                   HEREDOC
 
     assert_equal expected, Responder.game
+  end
+
+  def test_forbidden
+    assert_equal ['Forbidden', :forbidden], Responder.forbidden
+  end
+
+  def test_not_found
+    assert_equal ['Page not found', :not_found], Responder.not_found
+  end
+
+  def test_internal_error
+    assert_equal ['Internal Server Error', :error], Responder.internal_error
   end
 end
