64d72d94df0cfc76bd6d7032b8ebead1b4d3738a
pollockcl <pollock.clee@gmail.com>
2018-02-15 13:33:57 -0700

    added test for find_guess method

--- test/parser_test.rb
+++ test/parser_test.rb
@@ -5,14 +5,17 @@ require './lib/parser'
 class ParserTest < Minitest::Test
 
   def setup
-    request =["GET /find_word?word=barnacle HTTP/1.1",
+    @request =["GET /find_word?word=barnacle HTTP/1.1",
               "User-Agent: Faraday v0.14.0",
               "Accept-Encoding: gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
               "Accept: */*",
               "Connection: close",
               "Host: localhost:9292",
-              "GET / HTTP/1.1"]
-     @parse = Parser.new(request)
+              "GET / HTTP/1.1",
+              "Content-Length: 8"]
+
+     @client = File.open('./test/parser_client_test.txt')
+     @parse = Parser.new(@request)
   end
 
   def test_existance
@@ -31,5 +34,9 @@ class ParserTest < Minitest::Test
 
   def test_find_word_method
     assert_equal 'barnacle', @parse.find_word
+  end
+
+  def test_find_guess_method
+    assert_equal 78, @parse.find_guess(@client)
   end
 end
