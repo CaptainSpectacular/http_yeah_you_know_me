90f326612d6fdbe4502f7aa56ca2fad470dc8331
pollockcl <pollock.clee@gmail.com>
2018-02-15 14:08:18 -0700

    added tests for additional headers and fixed for tags

--- test/headers_test.rb
+++ test/headers_test.rb
@@ -10,6 +10,38 @@ class HeadersTest < Minitest::Test
                 "content-type: text/html; charset=iso-8859-1",
                 "content-length: 12\r\n\r\n"].join("\r\n")
 
-    assert_equal expected, Headers.default('twelve chars')
+    assert_equal expected, Headers.headers('twelve chars', :ok)
+  end
+
+  def test_other_headers
+    forbidden = ["http/1.1 403 Forbidden",
+                "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
+                "server: ruby",
+                "content-type: text/html; charset=iso-8859-1",
+                "content-length: 12\r\n\r\n"].join("\r\n")
+
+    not_found = ["http/1.1 404 Not Found",
+                "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
+                "server: ruby",
+                "content-type: text/html; charset=iso-8859-1",
+                "content-length: 12\r\n\r\n"].join("\r\n")
+
+    moved = ["http/1.1 302 Moved Permanently",
+             "Location: http://localhost:9292/game",
+             "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
+             "server: ruby",
+             "content-type: text/html; charset=iso-8859-1",
+             "content-length: 12\r\n\r\n"].join("\r\n")
+
+    error = ["http/1.1 500 Internal Server Error",
+             "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
+             "server: ruby",
+             "content-type: text/html; charset=iso-8859-1",
+             "content-length: 12\r\n\r\n"].join("\r\n")
+    
+    assert_equal forbidden, Headers.headers('twelve chars', :forbidden)
+    assert_equal not_found, Headers.headers('twelve chars', :not_found)
+    assert_equal moved, Headers.headers('twelve chars', :moved)
+    assert_equal error, Headers.headers('twelve chars', :error)
   end
 end
